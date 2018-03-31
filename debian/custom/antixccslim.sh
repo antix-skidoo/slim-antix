02700c43767a038a1152320120c47f6c714df5cc
antix-skidoo <antix-skidoo@users.noreply.github.com>
2018-03-31 20:06:46 +0000

    antixslimcc.sh: avoid external 'id' program call

--- debian/custom/antixccslim.sh
+++ debian/custom/antixccslim.sh
@@ -5,7 +5,7 @@
 
 # Check xserver is running and user is root
 [[ $DISPLAY ]] || { echo "There is no xserver running. Exiting..." ; exit 1 ; }
-[[ $(id -u) -eq 0 ]] || { yad --image "error" --title "!" --text "root (sudo) authorization required \! \n\nCannot continue." ; exit 1 ; }
+[ "$UID" !="0" ] || { yad --image "error" --title "!" --text "root (sudo) authorization required \! \n\nCannot continue." ; exit 1 ; }
 
 [[ -e /usr/local/bin/yad ]] || { echo "yad (provided by 'antix-goodies' package) missing; cannot proceed"; exit 1; }
 [[ -e /usr/bin/gtkdialog ]] || { echo "/usr/bin/gtkdialog missing; cannot proceed"; exit 1; }
